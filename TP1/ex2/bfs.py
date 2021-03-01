7d13d022b50661dad3271574a941a5c2b4a42555
Ruben Almeida <almeidaruben35@gmail.com>
2021-03-01 20:48:34 +0000

    ex2 not working yet

---
+++ TP1/ex2/bfs.py
@@ -0,0 +1,32 @@
+from state import State
+from operators import operators
+
+if __name__ == '__main__':
+    currState = State()
+
+    solution = [currState]
+    stateQueue = [currState]
+
+    while currState is not None:
+        if currState.is_final_state():
+            break
+
+        for operator in operators:
+            potential_state = operator(currState)
+
+            if potential_state is not None:
+                unique = True
+                for alreadyVisited in solution:
+                    if alreadyVisited == potential_state:
+                        unique = False
+                if unique:
+                    stateQueue.append(potential_state)
+
+        if len(stateQueue) == 0:
+            print("No solutions")
+            break
+
+        currState = stateQueue.pop(0)
+        solution.append(currState)
+
+    print(len(solution))
---
+++ TP1/ex2/state.py
@@ -0,0 +1,62 @@
+class State:
+
+    def __init__(self, number_miss_right=3, number_cannibals_right=3, boat_position="right"):
+        self._number_miss_right = number_miss_right
+        self._number_cannibals_right = number_cannibals_right
+        self._boat_position = boat_position
+        self._parent = None
+        self._depth = 0
+
+    def is_final_state(self):
+        return self._number_miss_right == 0 and self._number_cannibals_right == 0
+
+    @property
+    def boat_position(self):
+        return self._boat_position
+
+    @boat_position.setter
+    def boat_position(self, value):
+        self._boat_position = value
+
+    @property
+    def number_miss_right(self):
+        return self._number_miss_right
+
+    @number_miss_right.setter
+    def number_miss_right(self, value):
+        self._number_miss_right = value
+
+    @property
+    def number_cannibals_right(self):
+        return self._number_cannibals_right
+
+    @number_cannibals_right.setter
+    def number_cannibals_right(self, value):
+        self._number_miss_right = value
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, state):
+        self._parent = state
+        self._depth = state.depth + 1
+
+    def number_cannibals_left(self):
+        return 3 - self.number_cannibals_right
+
+    def number_miss_left(self):
+        return 3 - self.number_miss_right
+
+    @property
+    def depth(self):
+        return self._depth
+
+    @depth.setter
+    def depth(self, value):
+        self._depth = value
+
+    def __eq__(self, state):
+        return self.number_cannibals_right == state.number_cannibals_right \
+               and state.number_miss_right == self.number_miss_right and self.boat_position == state.boat_position
